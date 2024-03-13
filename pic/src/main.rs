3833ce4da20936b0c08e04b3d7a944397d02f251
jtabalon <joseph@tabalon.io>
2024-03-12 18:01:01 -0700

    implement clap

--- pic/src/main.rs
+++ pic/src/main.rs
@@ -1,3 +1,7 @@
+use clap::Parser;
+
+#[derive(Parser)]
+
 struct Cli {
     pattern: String,
     path: std::path::PathBuf,
@@ -5,13 +9,15 @@ struct Cli {
 
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
+    // let pattern = std::env::args().nth(1).expect("no pattern given");
+    // let path = std::env::args().nth(2).expect("no path given");
 
-    let args = Cli {
-        pattern: pattern,
-        path: std::path::PathBuf::from(path),
-    };
+    let args = Cli::parse();
+
+    // let args = Cli {
+    //     pattern: pattern,
+    //     path: std::path::PathBuf::from(path),
+    // };
 
     println!("pattern: {:?}, path: {:?}", args.pattern, args.path)
 }

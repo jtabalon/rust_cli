d5965f5519051d8fb899910b450fc6d2c9f3e1f6
jtabalon <joseph@tabalon.io>
2024-03-12 17:30:43 -0700

    initial arguments

---
+++ pic/target/debug/deps/pic-eab40534d0a4f1c1.d
@@ -0,0 +1,5 @@
+/Users/jtabalonjr/coding_projects/rust/rust_cli/pic/target/debug/deps/pic-eab40534d0a4f1c1: src/main.rs
+
+/Users/jtabalonjr/coding_projects/rust/rust_cli/pic/target/debug/deps/pic-eab40534d0a4f1c1.d: src/main.rs
+
+src/main.rs:
---
+++ pic/target/debug/pic.d
@@ -0,0 +1,1 @@
+/Users/jtabalonjr/coding_projects/rust/rust_cli/pic/target/debug/pic: /Users/jtabalonjr/coding_projects/rust/rust_cli/pic/src/main.rs
